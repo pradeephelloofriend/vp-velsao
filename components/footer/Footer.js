68d8bb72a27d4ab22dd4075e7aa151b5d725da49
Sanyog <sanyoggawas121@gmail.com>
2024-03-14 21:53:44 +0530

    velsao changes

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -30,28 +30,28 @@ import {selectFootData} from '../../redux/footer/footSelector';
                  <div className="container-fluid pb-2 mt-5 mt-md-5">
                     <div className='row'>
                         <div className='col-md-4'>
-                        <h6 className='mb-5 text-white text-decoration-underline'> Helpline Contact </h6>
-                                <p className='text-white'>Police – <b>100</b></p>
-                                <p className='text-white'>Fire – <b>101</b></p>
-                                <p className='text-white'>Ambulance –  <b>102</b></p>
-                                <p className='text-white'>Women Helpline - <b>1091</b></p>
-                                <p className='text-white'>Child Helpline - <b>1098</b></p>
+                        <h6 className='mb-2 text-white text-decoration-underline'> Helpline Contact </h6>
+                                <p className='mb-2 text-white'>Police – <b>100</b></p>
+                                <p className='mb-2 text-white'>Fire – <b>101</b></p>
+                                <p className='mb-2 text-white'>Ambulance –  <b>102</b></p>
+                                <p className='mb-2 text-white'>Women Helpline - <b>1091</b></p>
+                                <p className='mb-2 text-white'>Child Helpline - <b>1098</b></p>
                         </div>
                         <div className='col-md-4'>
-                        <h6 className='mb-5 text-white text-decoration-underline'> Quick Links </h6>
-                                <p onClick={() =>goToPage('reporting/admin-and-audit','admin-and-audit','Admin and Audit','Reporting','/admin-and-audit/','Yes')} className='text-white'>Admin & Audit</p>
-                                <p onClick={() =>goToPage('reporting/grants-received','grants-received','Grants Received','Reporting','/grants-received/','Yes')} className='text-white'>Grants Received</p>
-                                <p onClick={() =>goToPage('reporting/tenders','tenders','Tenders','Reporting','/tenders/','Yes')} className='text-white'>Tenders</p>
-                                <p onClick={() =>goToPage('reporting/applications','applications','Applications','Reporting','/applications/','Yes')} className='text-white'>Applications</p>
-                                <p onClick={() =>goToPage('about-us/panchayat-members','panchayat-members','Panchayat Members','About Us','/panchayat-members/','Yes')} className='text-white'>Members</p>
-                                <p onClick={() =>goToPage('about-us/projects','projects','Projects Members','About Us','/projects/','Yes')} className='text-white'>Projects</p>
+                        <h6 className='mb-2 text-white text-decoration-underline'> Quick Links </h6>
+                                <p onClick={() =>goToPage('reporting/admin-and-audit','admin-and-audit','Admin and Audit','Reporting','/admin-and-audit/','Yes')} className='mb-2 text-white'>Admin & Audit</p>
+                                <p onClick={() =>goToPage('reporting/grants-received','grants-received','Grants Received','Reporting','/grants-received/','Yes')} className='mb-2 text-white'>Grants Received</p>
+                                <p onClick={() =>goToPage('reporting/tenders','tenders','Tenders','Reporting','/tenders/','Yes')} className='mb-2 text-white'>Tenders</p>
+                                <p onClick={() =>goToPage('reporting/applications','applications','Applications','Reporting','/applications/','Yes')} className='mb-2 text-white'>Applications</p>
+                                <p onClick={() =>goToPage('about-us/panchayat-members','panchayat-members','Panchayat Members','About Us','/panchayat-members/','Yes')} className='mb-2 text-white'>Members</p>
+                                <p onClick={() =>goToPage('about-us/projects','projects','Projects Members','About Us','/projects/','Yes')} className='mb-2 text-white'>Projects</p>
                        
                         </div>
                         <div className='col-md-4'>
-                        <h6 className='mb-5 text-white text-decoration-underline'> Contact Details </h6>
-                                <p className='text-white'>Address:Village Panchayat Velsao Pale 120, MDR43, Consua, Goa 403712</p>
-                                <p className='text-white'>Phone number: +91 84089 16719</p>
-                                <p className='text-white'>Email: info@gmail.com</p>
+                        <h6 className='mb-2 text-white text-decoration-underline'> Contact Details </h6>
+                                <p className='mb-2 text-white'>Address:Village Panchayat Velsao Pale 120, MDR43, Consua, Goa 403712</p>
+                                <p className='mb-2 text-white'>Phone number: +91 7588533079</p>
+                                <p className='mb-2 text-white'>Email: panchayatvelsao@gmail.com</p>
                         </div>
                     </div>
                      
@@ -71,16 +71,16 @@ import {selectFootData} from '../../redux/footer/footSelector';
                         {/* <h6>Follow Us </h6> */}
 
                         <a target='_blank'>
-                            <i class="fa fa-instagram" aria-hidden="true"></i>
-                        </a> 
-                        <a target='_blank' href='https://www.facebook.com/profile.php?id=100090479850251'>
-                            <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                            <i class="fa fa-instagram" aria-hidden="false"></i>
                         </a> 
                         <a target='_blank'>
-                            <i class="fa fa-twitter-square" aria-hidden="true"></i>
+                            <i class="fa fa-facebook-official" aria-hidden="false"></i>
                         </a> 
                         <a target='_blank'>
-                            <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                            <i class="fa fa-twitter-square" aria-hidden="false"></i>
+                        </a> 
+                        <a target='_blank'>
+                            <i class="fa fa-youtube-play" aria-hidden="false"></i>
                         </a> 
                      </div>
 
@@ -107,7 +107,7 @@ import {selectFootData} from '../../redux/footer/footSelector';
                      
 
                      <div className="c-footer-pd text-center ft-rights">
-                        <p className="mb-2 mb-lg-0">© 2023 VPVelsaoPaleIssorcim. All rights reserved.</p>
+                        <p className="mb-2 mb-lg-0">© 2024 VP Velsao Pale Issorcim. All rights reserved.</p>
                      </div>
 
                      </div>
